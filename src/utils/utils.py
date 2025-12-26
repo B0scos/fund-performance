bf231c6e7fc5f2423a38e75ce7b24cf1f62aa7d2
B0scos <lol.lol.lol.matheus@gmail.com>
2025-12-26 08:57:23 -0300

    Better src/utils/utils.py

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,13 +1,18 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+from src.utils.custom_logger import get_logger
+from src.utils.custom_exception import CustomException
+
+
+logger = get_logger(__name__)
+
 def data_spliter(
     df: pd.DataFrame,
     val_cutoff: str,
     test_ratio: float = 0.2,
     date_col: str = "report_date",
     random_state: int = 42,
-    stratify_col: str | None = None
 ):
     """
     Split dataset into:
@@ -37,40 +42,25 @@ def data_spliter(
     val_df : pd.DataFrame
     """
 
-    if date_col not in df.columns:
-        raise ValueError(f"Column '{date_col}' not found in dataframe.")
-
-    if not 0 < test_ratio < 1:
-        raise ValueError("test_ratio must be between 0 and 1.")
-
     # Ensure datetime
     df = df.copy()
     df[date_col] = pd.to_datetime(df[date_col])
 
-    # Sort to avoid chaos
-    df = df.sort_values(date_col)
 
     # Validation split
     val_df = df[df[date_col] >= pd.to_datetime(val_cutoff)]
     train_test_df = df[df[date_col] < pd.to_datetime(val_cutoff)]
 
     if len(train_test_df) == 0:
-        raise ValueError("No data left for train/test before cutoff.")
-    if len(val_df) == 0:
-        print("Warning: validation set is empty.")
+        CustomException("No data left for train/test before cutoff.")
 
-    stratify = None
-    if stratify_col:
-        if stratify_col not in df.columns:
-            raise ValueError(f"Stratify column '{stratify_col}' not found.")
-        stratify = train_test_df[stratify_col]
+
 
     train_df, test_df = train_test_split(
         train_test_df,
         test_size=test_ratio,
         random_state=random_state,
         shuffle=True,  # time split already handled; this is ok within pre-cutoff
-        stratify=stratify
     )
 
     return train_df, test_df, val_df
